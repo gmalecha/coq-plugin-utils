32eb1c96d77500a0e5d18252eced5651cee60511
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-12-10 18:33:51 -0500

    single, slightly more general plugin to avoid ugly stuff.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 INSTALL_DIR=$(shell coqc -where)/user-contrib/PluginUtils
-INSTALL_FILES=plugin_utils.cma plugin_utils.cmxs plugin_utils.cmi
+INSTALL_FILES=plugin_utils.cma plugin_utils.cmx plugin_utils.cmi
 
 plugin:
 	$(MAKE) -C src
@@ -9,3 +9,10 @@ install: plugin
 	  install -d `dirname $(INSTALL_DIR)/$$i`; \
 	  install -m 0644 src/_build/$$i $(INSTALL_DIR)/$$i; \
 	done
+	install -m 0644 src/myocamlbuild.ml $(INSTALL_DIR)/myocamlbuild.ml
+
+uninstall:
+	for i in $(INSTALL_FILES); do \
+	  rm -f $(INSTALL_DIR)/$$i; \
+	done
+	rm -f $(INSTALL_DIR)/myocamlbuild.ml
--- src/myocamlbuild.ml
+++ src/myocamlbuild.ml
@@ -1,8 +1,51 @@
 open Ocamlbuild_plugin
-(*
-let coqlib = "/home/gmalecha/.opam/system/lib/coq/"
+open Unix
 
-let coq_dir s = coqlib ^ s
+let get_output cmd =
+  let ic, oc = Unix.open_process cmd in
+  let buf = Buffer.create 16 in
+  (try
+     while true do
+       Buffer.add_channel buf ic 1
+     done
+   with End_of_file -> ());
+  let _ = Unix.close_process (ic,oc) in
+  Buffer.contents buf
+
+let split ls sep =
+  let rec split_from n =
+    try
+      let x = String.index_from ls n sep in
+      if x > 0 then
+	String.sub ls n (x-n) :: split_from (x+1)
+      else
+	[String.sub ls n (String.length ls - n)]
+    with
+      Not_found ->
+	[String.sub ls n (String.length ls - n)]
+  in split_from 0
+
+let config =
+  let res =
+    lazy (let x = split (get_output "coqc -config") '\n' in
+          List.flatten (List.map
+			  (fun x ->
+			    match split x '=' with
+			      k :: v :: [] -> [(k,v)]
+			    | _ -> []) x)) in
+  fun x ->
+    try
+      Some (List.assoc x (Lazy.force res))
+    with
+      Not_found -> None
+
+let coqlib =
+  lazy (
+    match config "COQLIB" with
+      None -> assert false
+    | Some x -> x)
+
+let coq_dir s = Lazy.force coqlib ^ s
 
 let all_coq_dirs =
   ["kernel";"lib";"library";"parsing";"pretyping";
@@ -30,18 +73,37 @@ let all_coq_plugins =
   ;"plugins/syntax"
   ;"plugins/xml"]
 
-let coq_args = [A "-rectypes"] @ (List.flatten (List.map (fun x -> [A "-I"; A (coq_dir x)]) all_coq_dirs))
-let coq_plugin_args = [A "-rectypes"] @ (List.flatten (List.map (fun x -> [A "-I"; A (coq_dir x)]) all_coq_plugins))
+let coq_args =
+  [A "-rectypes"] @ (List.flatten (List.map (fun x -> [A "-I"; A (coq_dir x)]) all_coq_dirs))
+let coq_plugin_args =
+  [A "-rectypes"] @ (List.flatten (List.map (fun x -> [A "-I"; A (coq_dir x)]) all_coq_plugins))
+
+let coq_plugin_util_args =
+  List.flatten (List.map (fun x -> [A "-I"; A (coq_dir x)])
+		  ["user-contrib/PluginUtils"])
+
+let add_coq x =
+  match x with
+  | After_rules ->
+    rule ".ml4.ml" ~dep:"%.ml4" ~prod:"%.ml"
+      (fun env _ ->
+	let ml4 = env "%.ml4" and ml = env "%.ml" in
+	Cmd (S[A"camlp5o";T(tags_of_pathname ml4 ++ "p4mod");
+	       T(tags_of_pathname ml4 ++ "p4option");
+	       A"-I";A (coq_dir "parsing");
+	       A"pa_extend.cmo";A"pa_macro.cmo";A"q_MLast.cmo";A"grammar.cma";
+	       A"-loc";A"loc";
+	       A"-o"; Px ml; A"-impl"; P ml4])) ;
+    flag ["ocaml";"coq";"compile"] & (S coq_args) ;
+    flag ["ocaml";"coq";"pack"] & (S coq_args) ;
+    flag ["ocaml";"coq_plugins";"compile"] & (S coq_plugin_args) ;
+    flag ["ocaml";"coq_plugins";"pack"] & (S coq_plugin_args) ;
+    flag ["ocaml";"compile";"plugin_utils"] & (S coq_plugin_util_args) ;
+    flag ["ocaml";"link";"byte";"plugin_utils"] & (S (coq_plugin_util_args @ [A (coq_dir "user-contrib/PluginUtils/plugin_utils.cma")])) ;
+    flag ["ocaml";"link";"native";"plugin_utils"] & (S (coq_plugin_util_args @ [A (coq_dir "user-contrib/PluginUtils/plugin_utils.cmx")])) ;
+    pflag ["ocaml"] "cflags" (fun x -> S (List.map (fun x -> A x) (split x ','))) ;
+    pflag ["ocaml";"link"] "lflags" (fun x -> S (List.map (fun x -> A x) (split x ',')))
+  | _ -> ()
 ;;
 
-let add_coq = function
-  | After_rules ->
-    flag ["ocaml";"coq";"compile"] (S coq_args) ;
-    flag ["ocaml";"coq";"pack"] (S coq_args) ;
-    flag ["ocaml";"coq_plugins";"compile"] (S coq_plugin_args) ;
-    flag ["ocaml";"coq_plugins";"pack"] (S coq_plugin_args) ;
-  | _ -> ()
-*)
-open Coq_paths;;
-
-Ocamlbuild_plugin.dispatch Coq_paths.add_coq;;
+Ocamlbuild_plugin.dispatch add_coq
